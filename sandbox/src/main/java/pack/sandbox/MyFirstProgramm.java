a82baddfdd8e6f72802c0e88ead91e0beb9224a7
QAwayfarer <skdmitriev70@gmail.com>
2022-07-12 20:21:45 +0700

    Exercise

--- sandbox/src/main/java/pack/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/pack/sandbox/MyFirstProgramm.java
@@ -4,6 +4,7 @@ public class MyFirstProgramm {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println(1.0/2.0);
+        int l = 6;
+        System.out.println("S = " + (l * l));
     }
 }
