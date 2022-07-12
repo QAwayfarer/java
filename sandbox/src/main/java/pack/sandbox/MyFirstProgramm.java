aa821a1f5ff569379dc4d1e50de137f1cc7dc73c
QAwayfarer <skdmitriev70@gmail.com>
2022-07-12 20:47:12 +0700

    Functions

--- sandbox/src/main/java/pack/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/pack/sandbox/MyFirstProgramm.java
@@ -1,10 +1,32 @@
 package pack.sandbox;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 public class MyFirstProgramm {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        int l = 6;
-        System.out.println("S = " + (l * l));
+        hello("world");
+        hello("Sementiago");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " равна = " + area(l));
+
+        double a = 2;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна = " + area(a,b));
     }
+
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
