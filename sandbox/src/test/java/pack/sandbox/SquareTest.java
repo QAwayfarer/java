726f08e86b4b3d50e8ecf899606509137cd6e02f
QAwayfarer <skdmitriev70@gmail.com>
2022-07-16 15:12:31 +0700

    Добавлен первый автотест
    Проверка площади квадрата

---
+++ sandbox/src/test/java/pack/sandbox/SquareTest.java
@@ -0,0 +1,16 @@
+package pack.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SquareTest {
+
+    @Test
+    public void testArea (){
+        Square s = new Square(5);
+        s.area();
+        Assert.assertEquals(s.area(), 25);
+    }
+
+}
